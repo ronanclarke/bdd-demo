0dd40937497e9ddb7d4e0a7258fd9382f2bb4fda
Ronan Clarke <rclarke@whatclinic.com>
2014-04-16 16:00:21 +0100

    added plans

---
+++ db/migrate/20140416145835_add_demo_plans.rb
@@ -0,0 +1,15 @@
+class AddDemoPlans < ActiveRecord::Migration
+  def up
+    Plan.destroy_all
+
+    Plan.create(:name=>"Basic",:price=> 5, :kittens_per_month=>20)
+    Plan.create(:name=>"Premium",:price=> 30, :kittens_per_month=>50)
+    Plan.create(:name=>"Ultimate",:price=> 50, :kittens_per_month=>100)
+
+  end
+
+  def down
+    Plan.destroy_all
+
+  end
+end
